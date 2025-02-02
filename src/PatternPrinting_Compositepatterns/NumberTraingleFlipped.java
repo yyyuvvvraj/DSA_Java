4ee9630cbffc57d7348eb16e1832905e1bff3357
Yuvraj <yuvraj280605@gmail.com>
2025-02-02 10:36:46 +0530

    Initial commit

--- src/PatternPrinting_Compositepatterns/NumberTraingleFlipped.java
+++ src/PatternPrinting_Compositepatterns/NumberTraingleFlipped.java
@@ -1,4 +1,18 @@
 package PatternPrinting_Compositepatterns;
-
+import java.util.Scanner;
 public class NumberTraingleFlipped {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value of n:-");
+        int n=sc.nextInt();
+        for (int i=1;i<=n;i++){
+            for(int j=i;j<n;j++){
+                System.out.print(" "+" ");
+            }
+            for (int j=1;j<=i;j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
 }
