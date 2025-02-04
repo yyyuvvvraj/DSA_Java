1687ba6201d2cba0f9928afd592594b510ed9ae1
Yuvraj <yuvraj280605@gmail.com>
2025-02-04 15:44:49 +0530

    Initial commit

---
+++ src/PatternPrinitng/PatternPrinting_Compositepatterns/starDiamond.java
@@ -0,0 +1,35 @@
+package PatternPrinitng.PatternPrinting_Compositepatterns;
+import java.util.Scanner;
+public class starDiamond {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value of n:-");
+        int n=sc.nextInt();
+        int nsp=n-1;
+        int nst=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=nsp;j++){
+                System.out.print(" "+" ");
+            }
+            for (int j=1;j<=nst;j++){
+                System.out.print("*"+" ");
+            }
+            nsp--;
+            nst += 2;
+            System.out.println();
+        }
+        nsp=1;
+        nst=nst-4;
+        for(int i=1;i<=n-1;i++){
+            for(int j=1;j<=nsp;j++){
+                System.out.print(" "+" ");
+            }
+            for (int j=1;j<=nst;j++){
+                System.out.print("*"+" ");
+            }
+            nsp++;
+            nst -= 2;
+            System.out.println();
+        }
+    }
+}
