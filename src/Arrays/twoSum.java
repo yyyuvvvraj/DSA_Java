0d898cb71bac4de95ebcbb9f709da869c798da4e
Yuvraj <yuvraj280605@gmail.com>
2025-02-16 12:17:57 +0530

    Initial commit

---
+++ src/Arrays/twoSum.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+public class twoSum {
+    public static void main(String[] args) {
+        int[] arr={2,7,11,15};
+        int x=9;
+        int n=arr.length;
+        //sol
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(arr[i]+arr[j]==x){
+                    System.out.println(i+" "+j);
+                    break;
+                }
+            }
+        }
+    }
+}
