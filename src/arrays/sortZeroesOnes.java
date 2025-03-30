fa257e3a44061019144ac1d36b0c10b637e97843
Yuvraj <yuvraj280605@gmail.com>
2025-03-31 00:04:39 +0530

    Initial commit

--- src/arrays/sortZeroesOnes.java
+++ src/arrays/sortZeroesOnes.java
@@ -2,19 +2,38 @@ package arrays;
 
 public class sortZeroesOnes {
     public static void main(String[] args) {
-        int[] arr={1,0,0,1,1,0,0,0,1,0};
-        int n=arr.length;
-        int noOfZeroes=0,noOfOnes=0;
-        for(int i=0;i<n;i++){
-            if(arr[i]==0) noOfZeroes++;
-            else noOfOnes++;
+        int[] arr = {1, 0, 0, 1, 1, 0, 0, 0, 1, 0};
+        int n = arr.length;
+//        //Method-1
+//        int noOfZeroes = 0;
+//        for (int i = 0; i < n; i++) {
+//            if (arr[i] == 0) noOfZeroes++;
+//        }
+//        for (int i = 0; i < noOfZeroes; i++) {
+//            arr[i] = 0;
+//        }
+//        for (int i = noOfZeroes; i < n; i++) {
+//            arr[i] = 1;
+//        }
+//        for(int ele:arr){
+//            System.out.print(ele+" ");
+//        }
+//        System.out.println();
+//Method-2 ->One Pass Solution
+        int i=0,j=n-1;
+        while(i<j){
+            if(arr[i]==0) i++;
+            if(arr[j]==1)j--;
+            if(i<j && arr[i]==1&&arr[j]==0){
+                arr[i]=0;
+                arr[j]=1;
+                i++;
+                j--;
+            }
         }
-        for(int i=0;i<n;i++){
-            if(i<noOfZeroes)arr[i]=0;
-            else arr[i]=1;
+        for(int ele:arr){
+            System.out.print(ele+" ");
         }
-        for(int i=0;i<=n-1;i++){
-            System.out.print(arr[i]+" ");
-        }
+        System.out.println();
     }
 }
